f78dde2ddd9b9215984039feb451e70592797425
nandhiniusha96-afk <nandhiniusha96@gmail.com>
2026-06-21 18:34:33 +0530

    Update folder path for data upload in data_register.py

--- Tourism_Pkg_Prediction/model_building/data_register.py
+++ Tourism_Pkg_Prediction/model_building/data_register.py
@@ -19,7 +19,7 @@ except RepositoryNotFoundError:
     print(f"Space '{repo_id}' created.")
 
 api.upload_folder(
-    folder_path="AIML_MLOPS_TourismPkgPurchasePrediction/data",
+    folder_path="Tourism_Pkg_Prediction/data",
     repo_id=repo_id,
     repo_type=repo_type,
 )
